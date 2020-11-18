948d84b90fbc2f09687fc69589a90585ff5f4968
unknown <you@example.com>
2020-11-17 22:36:37 -0300

    Unit of work

--- ProjBiblio/ProjBiblio.Domain/Interfaces/IAutorRepository.cs
+++ ProjBiblio/ProjBiblio.Domain/Interfaces/IAutorRepository.cs
@@ -1,11 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using ProjBiblio.Domain.Entities;
 
-namespace ProjBiblio.Domain.Interfaces
-{
-    public interface IAutorRepository : IRepository<Autor>
-    {
+namespace ProjBiblio.Domain.Interfaces {    
+    public  interface  IAutorRepository :  IRepository<Autor>     {
+        IEnumerable<Autor> GetAutoresContemNome (string nome);
     }
 }
--- ProjBiblio/ProjBiblio.Domain/Interfaces/ILivroRepository.cs
+++ ProjBiblio/ProjBiblio.Domain/Interfaces/ILivroRepository.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using ProjBiblio.Domain.Entities;
 
-namespace ProjBiblio.Domain.Interfaces
-{
-    public interface ILivroRepository  : IRepository<Livro>
-    {
+namespace ProjBiblio.Domain.Interfaces {
+    public interface ILivroRepository :  IRepository<Livro> {
+        IEnumerable<Livro> GetLivrosSemEstoque ();
+        IEnumerable<Livro> GetLivrosContemTitulo (string titulo);
+        IEnumerable<Livro> GetLivrosPorAutor (int autorID);
     }
 }
